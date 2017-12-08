424ca2b159d85573268b2b8bb2dcb22627389787
thoffmann <toni.hoffmann@arthrex.de>
2017-12-08 14:16:43 +0100

    init block

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ var MessageType = {
 };
 
 var getGenesisBlock = () => {
-    return new Block(0, "0", 1465154705, "my genesis block!!", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7", 0);
+    return new Block(0, "0", 1465154705, "-> toni 100 coins", "4df43c5af7351f1476714e25be617b1ceab30f4bc48ec34c1b30e525478165a9", 10000);
 };
 
 var blockchain = [getGenesisBlock()];
